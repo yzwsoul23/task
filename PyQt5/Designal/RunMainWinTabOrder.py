4798ffa13cc1831df34214d14aff69ab8981d274
yzwsoul23 <2019520474@qq.com>
2021-06-23 18:00:02 +0800

    6.23

---
+++ PyQt5/Designal/RunMainWinTabOrder.py
@@ -0,0 +1,16 @@
+#导入相关模块
+import sys
+import MainWinTabOrder
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+if __name__ == '__main__':
+    # 创建QApplication类的实例
+    app = QApplication(sys.argv)
+    # 创建一个主窗口
+    mainwindow = QMainWindow()
+    # 调用ui文件里的类
+    ui = MainWinTabOrder.Ui_MainWindow()
+    # 向主窗口添加控件
+    ui.setupUi(mainwindow)
+    mainwindow.show()
+    sys.exit(app.exec_())
