80654d92d7925707f1c9819b929ee0286976c7c2
yzwsoul23 <2019520474@qq.com>
2021-07-04 22:28:23 +0800

    控件QSpinBox

---
+++ PyQt5/control/QSpinBoxDemo.py
@@ -0,0 +1,49 @@
+'''
+
+计数器控件（QSpinBox）
+
+
+'''
+
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class QSpinBoxDemo(QWidget):
+    def __init__(self):
+        super(QSpinBoxDemo,self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('QSpinBox演示')
+        self.resize(300,100)
+
+        layout = QVBoxLayout()
+        self.label = QLabel('当前值')
+        # 设置对齐模式
+        self.label.setAlignment(Qt.AlignCenter)
+
+        layout.addWidget(self.label)
+
+        self.sb = QSpinBox()
+        # 设置初始值
+        self.sb.setValue(5)
+        # 设置计数器范围
+        self.sb.setRange(-10,101)
+        # 设置单次调整步数(步进值)
+        self.sb.setSingleStep(50)
+        layout.addWidget(self.sb)
+        # 连接值被更改信号和valueChange槽函数
+        self.sb.valueChanged.connect(self.valueChange)
+        self.setLayout(layout)
+
+    def valueChange(self):
+        # 将标签文本设置为计数器的值
+        self.label.setText('当前值：' + str(self.sb.value()))
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = QSpinBoxDemo()
+    main.show()
+    sys.exit(app.exec_())
