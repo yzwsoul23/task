72eb98bee170db60534fd757d1a5e5954047cd82
yzwsoul23 <2019520474@qq.com>
2021-07-06 21:20:01 +0800

    7.6

---
+++ PyQt5/control/password.py
@@ -0,0 +1,45 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import sys
+
+class PassWord(QWidget):
+    def __init__(self):
+        super(PassWord, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.edit = QLineEdit()
+        self.edit.setPlaceholderText('请输入密码')
+        self.edit.setEchoMode(QLineEdit.Password)
+        self.edit.textEdited.connect(self.txt)
+
+        self.button = QPushButton('显示')
+        self.button.setCheckable(True)
+        self.button.toggle()
+        self.button.clicked.connect(self.buttonState)
+
+        hboxlayou = QHBoxLayout()
+        hboxlayou.addWidget(self.edit)
+        hboxlayou.addWidget(self.button)
+        self.setLayout(hboxlayou)
+        self.setWindowTitle('输入密码')
+
+    def buttonState(self):
+        if self.button.isChecked():
+            self.edit.setEchoMode(QLineEdit.Normal)
+        else:
+            self.edit.setEchoMode(QLineEdit.Password)
+
+    def txt(self, text):
+        if text == '2019520474':
+            self.setWindowTitle('密码正确')
+        else:
+            self.setWindowTitle('密码错误')
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = PassWord()
+    main.show()
+    sys.exit(app.exec_())
