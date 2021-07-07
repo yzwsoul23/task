4a6c8bf4b574bf6fc5f3dbfcacd075c8f0037569
yzwsoul23 <2019520474@qq.com>
2021-07-07 23:38:14 +0800

    7.7

--- PyQt5/control/password.py
+++ PyQt5/control/password.py
@@ -13,7 +13,7 @@ class PassWord(QWidget):
         self.edit.setPlaceholderText('请输入密码')
         self.edit.setEchoMode(QLineEdit.Password)
         self.edit.setMinimumSize(200,20)
-        self.edit.textEdited.connect(self.txt)
+        self.edit.returnPressed.connect(self.txt)
         # 字符和数字
         reg = QRegExp('[a-zA-Z0-9]+$')  # 正则表达式
         validator = QRegExpValidator(self)
@@ -39,11 +39,29 @@ class PassWord(QWidget):
     def buttonrelease(self):
         self.edit.setEchoMode(QLineEdit.Password)
 
-    def txt(self, text):
-        if text == '2019520474':
-            self.setWindowTitle('密码正确')
+    def txt(self):
+        if self.edit.text == '2019520474':
+            dialog = QDialog()
+            dialog.resize(50,50)
+            button = QPushButton('密码正确',dialog)
+            button.move(25, 25)
+            dialog.setWindowTitle('密码正确✔')
+            # 设置窗口显示模式不关掉对话框主窗口就无法操作
+            dialog.setWindowModality(Qt.ApplicationModal)
+            # 显示对话框
+            dialog.exec()
         else:
-            self.setWindowTitle('密码错误')
+            dialog = QDialog()
+            dialog.resize(50, 75)
+            button = QPushButton('重新输入', dialog)
+            button.move(23,25)
+            button.clicked.connect(self.edit.clear)
+            button.clicked.connect(dialog.close)
+            dialog.setWindowTitle('密码错误✘')
+            # 设置窗口显示模式不关掉对话框主窗口就无法操作
+            dialog.setWindowModality(Qt.ApplicationModal)
+            # 显示对话框
+            dialog.exec()
 
 
 if __name__ == '__main__':
